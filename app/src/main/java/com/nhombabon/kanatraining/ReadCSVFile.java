f4a1cb33f12259ff10ca5827bba8aec4a2e8de2b
Duc Tien <ductien.uit@gmail.com>
2017-11-03 23:17:43 +0700

    add class Read CSV file

---
+++ app/src/main/java/com/nhombabon/kanatraining/ReadCSVFile.java
@@ -0,0 +1,44 @@
+package com.nhombabon.kanatraining;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* compiled from: Common */
+class ReadCSVFile {
+    InputStream inputStream;
+
+    public ReadCSVFile(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    public List<String[]> read() {
+        List<String[]> resultList = new ArrayList();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(this.inputStream));
+        while (true) {
+            try {
+                String csvLine = reader.readLine();
+                if (csvLine == null) {
+                    try {
+                        this.inputStream.close();
+                        return resultList;
+                    } catch (IOException e) {
+                        throw new RuntimeException("Error while closing input stream: " + e);
+                    }
+                }
+                resultList.add(csvLine.split(","));
+            } catch (IOException e2) {
+                throw new RuntimeException("Error in reading CSV file: " + e2);
+            } catch (Throwable th) {
+                try {
+                    this.inputStream.close();
+                } catch (IOException e22) {
+                    throw new RuntimeException("Error while closing input stream: " + e22);
+                }
+            }
+        }
+    }
+}
