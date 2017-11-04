d7c4c22717f34df95467825637c780cc275883b7
Duc Tien <ductien.uit@gmail.com>
2017-11-04 22:40:21 +0700

    add xml and activity QuizMain and some sml for style

---
+++ app/src/main/java/com/nhombabon/kanatraining/QuizMainActivity.java
@@ -0,0 +1,13 @@
+package com.nhombabon.kanatraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_main);
+    }
+}
